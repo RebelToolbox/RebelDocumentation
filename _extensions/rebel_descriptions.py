23312b6abc3a9b82d95dce1e27ae35515237ff83
Marcel Admiraal <madmiraal@users.noreply.github.com>
2023-09-26 11:48:46 +0200

    Update conf.py

--- _extensions/rebel_descriptions.py
+++ _extensions/rebel_descriptions.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-    godot_descriptions
+    rebel_descriptions
     ~~~~~~~~~~~~~~~~~~
 
     Sphinx extension to automatically generate HTML meta description tags
-    for all pages. Also comes with some special support for Godot class docs.
+    for all pages. Also comes with some special support for Rebel class docs.
 
-    :copyright: Copyright 2021 by The Godot Engine Community
+    :copyright: Copyright 2023 by The Rebel Toolbox Contributors
     :license: MIT.
 
     based on the work of Takayuki Shimizukawa on OpenGraph support for Sphinx,
