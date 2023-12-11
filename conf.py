49e8a82c23bb39dc83c2db4276a2285f90e1f475
Marcel Admiraal <madmiraal@users.noreply.github.com>
2023-12-11 14:56:19 +0100

    Clean up conf.py

--- conf.py
+++ conf.py
@@ -1,175 +1,126 @@
-# -*- coding: utf-8 -*-
+# Sphinx build configuration file for Rebel Documentation.
 #
-# Rebel Toobox Documentation build configuration file
+# Contents
+# 1. Project information
+# 2. General configuration
+# 3. Options for internationalization
+# 4. Options for HTML output
+# 5. Options for HTML help output
+# 6. Options for the linkcheck builder
+# 7. Options for Docutils
+#   7.1 restructuredText Directives
+# 8. Extensions
+#   8.1 Sphinx tabs extension
+#   8.2 Sphinx not found page extension
+#   8.3 Sphinx opengraph extension
 
 import sphinx_rtd_theme
 import sys
 import os
 
-# -- General configuration ------------------------------------------------
 
-needs_sphinx = "1.3"
+# 1. Project information
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-# Sphinx extension module names and templates location
-sys.path.append(os.path.abspath("_extensions"))
+project = "Rebel Documentation"
+author = "Rebel Documentation Contributors"
+copyright = "2023, Rebel Documentation Contributors"
+version = os.getenv("READTHEDOCS_VERSION", "1.0")
+release = version
+
+
+# 2. General configuration
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+# https://www.sphinx-doc.org/en/master/usage/extensions/
+sys.path.append("_extensions")
 extensions = [
+    "gdscript",
+    "sphinx_rtd_theme",
     "sphinx_tabs.tabs",
     "notfound.extension",
     "sphinxext.opengraph",
 ]
 
-# Warning when the Sphinx Tabs extension is used with unknown
-# builders (like the dummy builder) - as it doesn't cause errors,
-# we can ignore this so we still can treat other warnings as errors.
-sphinx_tabs_nowarn = True
+exclude_patterns = [
+    "_build",
+    "env",
+]
 
-# Custom 4O4 page HTML template.
-# https://github.com/readthedocs/sphinx-notfound-page
-notfound_context = {
-    "title": "Page not found",
-    "body": """
-        <h1>Page not found</h1>
-        <p>
-            Sorry, we couldn't find that page. It may have been renamed or removed
-            in the version of the documentation you're currently browsing.
-        </p>
-        <p>
-            If you're currently browsing the
-            <em>stable</em> version of the documentation, try browsing the
-            <a href="/en/latest/"><em>latest</em> version of the documentation</a>.
-        </p>
-        <p>
-            Alternatively, use the
-            <a href="#" onclick="$('#rtd-search-form [name=\\'q\\']').focus()">Search docs</a>
-            box on the left or <a href="/">go to the homepage</a>.
-        </p>
-    """,
-}
+templates_path = [
+    "_templates",
+]
 
-# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
-on_rtd = os.environ.get("READTHEDOCS", None) == "True"
+smartquotes = False
 
-# Don't add `/en/latest` prefix during local development.
-# This makes it easier to test the custom 404 page by loading `/404.html`
-# on a local web server.
-if not on_rtd:
-    notfound_urls_prefix = ''
-
-# Specify the site name for the Open Graph extension.
-ogp_site_name = "Rebel Toolbox Documentation"
-
-if not os.getenv("SPHINX_NO_GDSCRIPT"):
-    extensions.append("gdscript")
-
-if not os.getenv("SPHINX_NO_DESCRIPTIONS"):
-    extensions.append("rebel_descriptions")
-
-templates_path = ["_templates"]
-
-# You can specify multiple suffix as a list of string: ['.rst', '.md']
-source_suffix = ".rst"
-source_encoding = "utf-8-sig"
-
-# The master toctree document
-master_doc = "index"
-
-# General information about the project
-project = "Rebel Toolbox"
-copyright = "2023, Rebel Documentation Contributors"
-author = "Rebel Documentation Contributors"
-
-# Version info for the project, acts as replacement for |version| and |release|
-# The short X.Y version
-version = os.getenv("READTHEDOCS_VERSION", "1.0")
-# The full version, including alpha/beta/rc tags
-release = version
-
-# Parse Sphinx tags passed from RTD via environment
-env_tags = os.getenv("SPHINX_TAGS")
-if env_tags is not None:
-    for tag in env_tags.split(","):
-        print("Adding Sphinx tag: %s" % tag.strip())
-        tags.add(tag.strip())  # noqa: F821
-
-# Language / i18n
-
-supported_languages = {
-    "en": "Rebel Toolbox (%s) documentation in English",
-}
-
-language = os.getenv("READTHEDOCS_LANGUAGE", "en")
-if not language in supported_languages.keys():
-    print("Unknown language: " + language)
-    print("Supported languages: " + ", ".join(supported_languages.keys()))
-    print(
-        "The configured language is either wrong, or it should be added to supported_languages in conf.py. Falling back to 'en'."
-    )
-    language = "en"
-
-is_i18n = tags.has("i18n")  # noqa: F821
-
-exclude_patterns = ["_build","env"]
-
-# fmt: off
-# These imports should *not* be moved to the start of the file,
-# they depend on the sys.path.append call registering "_extensions".
-# GDScript syntax highlighting
 from gdscript import GDScriptLexer
 from sphinx.highlighting import lexers
 
 lexers["gdscript"] = GDScriptLexer()
-# fmt: on
-
-smartquotes = False
-
-# Pygments (syntax highlighting) style to use
-pygments_style = "sphinx"
 highlight_language = "gdscript"
 
-# -- Options for HTML output ----------------------------------------------
+
+# 3. Options for internationalization
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-internationalization
+
+# TODO Rebel internationalisation needs to configured.
+# https://www.sphinx-doc.org/en/master/usage/advanced/intl.html
+# https://readthedocs-lst.readthedocs.io/en/latest/i18n.html
+# https://readthedocs-lst.readthedocs.io/en/latest/localization.html
+
+language = os.getenv("READTHEDOCS_LANGUAGE", "en")
+
+supported_languages = {
+    "en": "Rebel Toolbox (%s) documentation in English",
+}
+if language not in supported_languages.keys():
+    print("Unknown language: " + language)
+    print("Supported languages: " + ", ".join(supported_languages.keys()))
+    print(
+        "The configured language is either wrong,"
+        + " or it should be added to supported_languages in conf.py."
+        + " Falling back to 'en'."
+    )
+    language = "en"
+
+
+# 4. Options for HTML output
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-if on_rtd:
-    using_rtd_theme = True
 
-# Theme options
+# https://sphinx-rtd-theme.readthedocs.io/en/stable/configuring.html
 html_theme_options = {
-    # if we have a html_logo below, this shows /only/ the logo with no title text
-    "logo_only": True,
-    # Collapse navigation (False makes it tree-like)
     "collapse_navigation": False,
+    "logo_only": True,
 }
+
+html_theme_path = [
+    sphinx_rtd_theme.get_html_theme_path(),
+]
 
 html_title = supported_languages[language] % version
 
-# VCS options: https://docs.readthedocs.io/en/latest/vcs.html#github
 html_context = {
-    "display_github": not is_i18n,  # Integrate GitHub
-    "github_user": "RebelToolbox",  # Username
-    "github_repo": "RebelDocumentation",  # Repo name
-    "github_version": "main",  # Version
-    "conf_py_path": "/",  # Path in the checkout to the docs root
+    # https://readthedocs-lst.readthedocs.io/en/latest/vcs.html
+    # Manually set Read the Docs variables for local builds.
+    "github_user": "RebelToolbox",  # GitHub username.
+    "github_repo": "RebelDocumentation",  # Github repo name.
+    "github_version": "main",  # Version.
+    "conf_py_path": "/",  # Path in the checkout to the docs root.
+    "display_github": True,
+    # Define variables used in _templates/layout.html.
     "inject_language_links": True,
     "docs_supported_languages": list(supported_languages.keys()),
     "docs_basepath": "https://docs.rebeltoolbox.com/",
     "docs_suffix": ".html",
     "default_lang": "en",
     "canonical_version": "stable",
-    # Distinguish local development website from production website.
-    # This prevents people from looking for changes on the production website after making local changes :)
-    "title_prefix": "" if on_rtd else "(DEV) ",
+    "title_prefix": "",
 }
 
 html_logo = "img/rebel-logo.png"
 
-# These folders are copied to the documentation's HTML output
-html_static_path = ["_static"]
-
-html_extra_path = ["robots.txt"]
-
-# These paths are either relative to html_static_path
-# or fully qualified paths (e.g. https://...)
 html_css_files = [
     "css/custom.css",
 ]
@@ -178,100 +129,71 @@ html_js_files = [
     "js/custom.js",
 ]
 
-# Output file base name for HTML help builder
-htmlhelp_basename = "RebelDocumentation"
-
-# -- Options for reStructuredText parser ----------------------------------
-
-# Enable directives that insert the contents of external files
-file_insertion_enabled = False
-
-# -- Options for LaTeX output ---------------------------------------------
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (
-        master_doc,
-        "RebelDocumentation.tex",
-        "Rebel Toolbox Documentation",
-        "Rebel Toolbox Contributors",
-        "manual",
-    ),
+html_static_path = [
+    "_static",
 ]
 
-# -- Options for linkcheck builder ----------------------------------------
 
-# disable checking urls with about.html#this_part_of_page anchors
+# 5. Options for HTML help output
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-help-output
+
+htmlhelp_basename = "RebelDocumentation"
+
+
+# 6. Options for the linkcheck builder
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-the-linkcheck-builder
+
 linkcheck_anchors = False
-
 linkcheck_timeout = 10
 
-# -- I18n settings --------------------------------------------------------
 
-# TODO Rebel localization needs to be forked from https://github.com/godotengine/godot-docs-l10n
-# Godot localization is handled via https://github.com/godotengine/godot-docs-l10n
-# There the main docs repo is a submodule. Therefore the translated material is
-# actually in the parent folder of this conf.py, hence the "../".
+# 7. Options for Docutils
+# https://www.docutils.org/docs/user/config.html
 
-locale_dirs = ["../sphinx/po/"]
-gettext_compact = False
+# 7.1 restructuredText Directives
+# https://www.docutils.org/docs/ref/rst/directives.html
 
-# We want to host the localized images in godot-docs-l10n, but Sphinx does not provide
-# the necessary feature to do so. `figure_language_filename` has `{root}` and `{path}`,
-# but they resolve to (host) absolute paths, so we can't use them as is to access "../".
-# However, Python is glorious and lets us redefine Sphinx's internal method that handles
-# `figure_language_filename`, so we do our own post-processing to fix the absolute path
-# and point to the parallel folder structure in godot-docs-l10n.
-# Note: Sphinx's handling of `figure_language_filename` may change in the future, monitor
-# https://github.com/sphinx-doc/sphinx/issues/7768 to see what would be relevant for us.
-figure_language_filename = "{root}.{language}{ext}"
+# To avoid potential security holes in the csv-table, include and
+# raw directives file_insertion and raw are disabled.
+file_insertion_enabled = False
+raw_enabled = False
 
-import sphinx
-cwd = os.getcwd()
+# 8. Extensions
 
-sphinx_original_get_image_filename_for_language = sphinx.util.i18n.get_image_filename_for_language
+# 8.1 Sphinx tabs extension
+# https://sphinx-tabs.readthedocs.io
+# Required by Read the Docs
 
-def rebel_get_image_filename_for_language(filename, env):
-    """
-    Hack the absolute path returned by Sphinx based on `figure_language_filename`
-    to insert our `../images` relative path to godot-docs-l10n's images folder,
-    which mirrors the folder structure of the docs repository.
-    The returned string should also be absolute so that `os.path.exists` can properly
-    resolve it when trying to concatenate with the original doc folder.
-    """
-    path = sphinx_original_get_image_filename_for_language(filename, env)
-    path = os.path.abspath(os.path.join("../images/", os.path.relpath(path, cwd)))
-    return path
+sphinx_tabs_nowarn = True
 
-sphinx.util.i18n.get_image_filename_for_language = rebel_get_image_filename_for_language
 
-# Similar story for the localized class reference, it's out of tree and there doesn't
-# seem to be an easy way for us to tweak the toctree to take this into account.
-# So we're deleting the existing class reference and adding a symlink instead...
-if is_i18n and os.path.exists("../classes/" + language):
-    import shutil
+# 8.2 Sphinx not found extension
+# https://sphinx-notfound-page.readthedocs.io
 
-    if os.path.islink("classes"):  # Previously made symlink.
-        os.unlink("classes")
-    else:
-        shutil.rmtree("classes")
+notfound_context = {
+    "title": "Page not found",
+    "body": """
+        <h1>Page not found</h1>
+        <p>
+            Sorry, we couldn't find that page. It may have been renamed or
+            removed in the version of the documentation you're currently
+            browsing.
+        </p>
+        <p>
+            If you're currently browsing the <em>stable</em> version of the
+            documentation, try browsing the
+            <a href="/en/latest/"><em>latest</em> version of the
+            documentation</a>.
+        </p>
+        <p>
+            Alternatively, use the <a href="#"
+            onclick="$('#rtd-search-form [name=\\'q\\']').focus()">Search
+            docs</a> box on the left or <a href="/">go to the homepage</a>.
+        </p>
+    """,
+}
 
-    os.symlink("../classes/" + language, "classes")
+# 8.3 Sphinx opengraph extension
+# https://github.com/wpilibsuite/sphinxext-opengraph
 
-# Couldn't find a way to retrieve variables nor do advanced string
-# concat from reST, so had to hardcode this in the "epilog" added to
-# all pages. This is used in index.rst to display the Weblate badge.
-# On English pages, the badge points to the language-neutral engage page.
-# TODO Update once Rebel Toolbox Weblate project is active
-#rst_epilog = """
-#.. |weblate_widget| image:: https://hosted.weblate.org/widgets/godot-engine/{image_locale}/godot-docs/287x66-white.png
-#    :alt: Translation status
-#    :target: https://hosted.weblate.org/engage/godot-engine{target_locale}/?utm_source=widget
-#    :width: 287
-#    :height: 66
-#""".format(
-#    image_locale="-" if language == "en" else language,
-#    target_locale="" if language == "en" else "/" + language,
-#)
+ogp_site_name = "Rebel Toolbox Documentation"
